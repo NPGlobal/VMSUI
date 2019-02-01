45af227fa0143985ee952cd8ad398c491c37373a
Shubhi-J <45873056+Shubhi-J@users.noreply.github.com>
2019-02-01 14:44:20 +0530

    To bind dropdown.

--- src/app/Components/staff-details/staff-details.component.ts
+++ src/app/Components/staff-details/staff-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, FormControl,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { vendorStaff } from 'src/app/Models/vendorStaff';
 import { VendorService } from 'src/app/Services/vendor.service';
 import { HttpClient } from '@angular/common/http';
@@ -14,65 +14,77 @@ declare var $: any;
 export class StaffDetailsComponent implements OnInit {
   VendorStaff: vendorStaff;
   staffDetailsForm: FormGroup;
-
+  deptList: any[];
+  designationList: any[];
   status = true;
-  submitted=false;
+  submitted = false;
   constructor(private _vendorService: VendorService,
     private _route: ActivatedRoute,
     private _fb: FormBuilder) { }
-  
-    ngOnInit() 
-    {
-      
-      this.staffDetailsForm= this._fb.group({
-        dept: ['', Validators.required],
-        designation: ['', Validators.required],
-        name: ['', Validators.required],
-        email:['', Validators.email],
-        phone:['', [Validators.minLength(10),Validators.maxLength(10)]],
-        priority:['', Validators.required],
-        status:true,
-        remarks:'',
-        IsExpanded:true
-        
-       
-      }); 
-    }
 
-    InitializeFormControls() {
+  ngOnInit() {
+    this.GetVendorDepartments();
 
-      this.staffDetailsForm = this._fb.group({
-          dept:[this.VendorStaff.dept],
-          designation:[this.VendorStaff.designation],
-          name: [this.VendorStaff.name],
-          email: [this.VendorStaff.email],
-          phone: [this.VendorStaff.phone],
-          priority: [this.VendorStaff.priority],
-          status: [this.VendorStaff.status],
-          remarks: [this.VendorStaff.remarks],
-          IsExpanded: true
-     });
-    }
-  
-    ToggleContainer(formGroup: FormGroup) {
-      formGroup.controls.IsExpanded.patchValue(!formGroup.controls.IsExpanded.value);
-    }
-  SaveStaffDetails()
-  {
-    this.submitted=true;
-    //alert(1);
+    this.staffDetailsForm = this._fb.group({
+      dept: ['', Validators.required],
+      designation: ['', Validators.required],
+      name: ['', Validators.required],
+      email: ['', Validators.email],
+      phone: ['', [Validators.minLength(10), Validators.maxLength(10)]],
+      priority: ['', Validators.required],
+      status: true,
+      remarks: '',
+      IsExpanded: true
+
+
+    });
+  }
+  GetVendorDepartments() {
+    this._vendorService.GetVendorsDeptStaff('10', '-1', 'Department').subscribe((data) => {
+      this.deptList = data;
+    });
+  }
+  GetVendorDesignation() {
+
+    // console.log(this.staffDetailsForm.get('dept').value);
+    this._vendorService.GetVendorDesignation('10', this.staffDetailsForm.get('dept').value, 'Designation').subscribe((data) => {
+      this.designationList = data;
+    });
+  }
+
+  InitializeFormControls() {
+
+    this.staffDetailsForm = this._fb.group({
+      dept: [this.VendorStaff.dept],
+      designation: [this.VendorStaff.designation],
+      name: [this.VendorStaff.name],
+      email: [this.VendorStaff.email],
+      phone: [this.VendorStaff.phone],
+      priority: [this.VendorStaff.priority],
+      status: [this.VendorStaff.status],
+      remarks: [this.VendorStaff.remarks],
+      IsExpanded: true
+    });
+  }
+
+  ToggleContainer(formGroup: FormGroup) {
+    formGroup.controls.IsExpanded.patchValue(!formGroup.controls.IsExpanded.value);
+  }
+
+  SaveStaffDetails() {
+    this.submitted = true;
+    // alert(1);
     if (this.staffDetailsForm.invalid) {
-      alert("something went wrong");
-        return;
-      }
-      console.log(JSON.stringify(this.staffDetailsForm.value));
-      alert(JSON.stringify(this.staffDetailsForm.value));
-      //console.log(JSON.stringify(this.addressForm));
-      this._vendorService.SaveStaffInfo(this.staffDetailsForm.value);
-      alert('SUCCESS!! :-)')
+      alert('something went wrong');
+      return;
     }
-    //alert(1);
-    //alert(JSON.stringify(this.staffDetailsForm.value));
-  
+    console.log(JSON.stringify(this.staffDetailsForm.value));
+    alert(JSON.stringify(this.staffDetailsForm.value));
+    // console.log(JSON.stringify(this.addressForm));
+    this._vendorService.SaveStaffInfo(this.staffDetailsForm.value);
+    alert('SUCCESS!! :-)');
+  }
+  // alert(1);
+  // alert(JSON.stringify(this.staffDetailsForm.value));
 }
 
--- src/app/Services/vendor.service.ts
+++ src/app/Services/vendor.service.ts
@@ -11,19 +11,26 @@ export class VendorService {
   constructor(private _http: HttpClient) { }
 
   GetVendors(pageIndex: number, Limit: number): Observable<any> {
-    const apiUrl = this.origin + 'v1/Vendor/GetVendors/' + pageIndex + '/' + Limit;
+    const apiUrl = this.origin + 'api/Vendor/GetVendors/' + pageIndex + '/' + Limit;
     return this._http.get<any>(apiUrl);
   }
 
   GetVendorByCode(code: string): Observable<any> {
-    const apiUrl = this.origin + 'v1/Vendor/GetVendorByCode/' + code;
+    const apiUrl = this.origin + 'api/Vendor/GetVendorByCode/' + code;
     return this._http.get<any>(apiUrl);
   }
 
-  //Added by Shubhi
+  // Added by Shubhi
   SaveStaffInfo(staffForm: any): Observable<any> {
-    const apiUrl = this.origin+'SaveVendorStaff';
-    return this._http.post<any>(apiUrl,staffForm);
+    const apiUrl = this.origin + 'SaveVendorStaff';
+    return this._http.post<any>(apiUrl, staffForm);
   }
-
+  GetVendorsDeptStaff(companycode: string, deptcode: string, type: string): Observable<any> {
+    const apiUrl = this.origin + 'api/VendorStaff/GetVendorsDeptStaff/' + companycode + '/' + deptcode + '/' + type;
+    return this._http.get<any>(apiUrl);
+  }
+  GetVendorDesignation(companycode: string, deptcode: string, type: string): Observable<any> {
+    const apiUrl = this.origin + 'api/VendorStaff/GetVendorsDeptStaff/' + companycode + '/' + deptcode + '/' + type;
+    return this._http.get<any>(apiUrl);
+  }
 }
