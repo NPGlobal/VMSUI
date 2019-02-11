41aae0e3dd8cd48e302599096ffc756db603f9c5
ashutoshchaturvediNP <ashutosh.chaturvedi@netprophetsglobal.com>
2019-02-11 18:20:27 +0530

    Modified business unit

---
+++ src/app/Models/vendor-business-details.ts
@@ -0,0 +1,19 @@
+export class VendorBusinessDetails {
+    VendorBusinessDetailsID: number;
+    FinancialYear: string;
+    CompanyCode: string;
+    VendorCode: string;
+    divisionCode: string;
+    deptCode: string;
+    ActualDPGrnQty: number;
+    ActualDPValueQty: number;
+    ActualJWGrnQty: number;
+    ActualJWValueQty: number;
+    ProposedDPGrnQty: number;
+    ProposedDPValueQty: number;
+    ProposedJWGrnQty: number;
+    ProposedJWValueQty: number;
+    Status: string;
+    Remarks: string;
+    CreatedBy: number;
+}
--- src/app/Services/vendor.service.ts
+++ src/app/Services/vendor.service.ts
@@ -101,4 +101,20 @@ export class VendorService {
     const apiUrl = this.origin + 'api/MasterVendor/GetMasterVendorList';
     return this._http.get<any>(apiUrl);
   }
+
+  // Used for save and update Vendor Business Info
+  SaveVendorBusinessInfo(VendorBusiness: any): Observable<any> {
+    const apiUrl = this.origin + 'api/VendorBusiness/SaveVendorBusinessInfo';
+    return this._http.post<any>(apiUrl, VendorBusiness);
+  }
+  // Get Vendor Business Information for Edit
+  GetBusinessDetails(id: number): Observable<any> {
+    const apiUrl = this.origin + 'api/VendorBusiness/GetBusinessDetails/' + id;
+    return this._http.get<any>(apiUrl);
+  }
+   // Get all Staff of a Vendor
+   GetVendorBusinessByVendorCode(vcode: string, pageIndex: number, Limit: number): Observable<any> {
+    const apiUrl = this.origin + 'api/VendorBusiness/GetVendorBusinessByVendorCode/10/' + vcode + '/' + pageIndex + '/' + Limit;
+    return this._http.get<any>(apiUrl);
+  }
 }
