f088867440c604dfb61adff19843b5e47d97dfc4
arvindsharmaNP <45871965+arvindsharmaNP@users.noreply.github.com>
2019-02-12 11:21:48 +0530

    add new model for production

---
+++ src/app/Models/VendorProduction.ts
@@ -0,0 +1,17 @@
+export class VendorProduction{
+    id: number;
+
+    division:string;
+    department:string;
+
+    approvedProductionUnits: number;
+    subContractingUnitName:string;
+    subContractingUnitAddress:string;
+    natureOfSubContractingUnit: string;
+    
+    monthlyCapacity: number;
+    minimalCapacity: number;
+    leanMonths: number;
+    leanCapicity:number;
+
+}
