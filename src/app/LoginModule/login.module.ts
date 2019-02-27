9b205a929e247f2ea6a83d64d779ab3210ec3d11
unikedison <45872111+unikedison@users.noreply.github.com>
2019-02-27 12:31:25 +0530

    Validation active on Submit

--- src/app/LoginModule/login.module.ts
+++ src/app/LoginModule/login.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VendorListComponent } from '../Components/vendor-list/vendor-list.component';
 import { AddVendorComponent } from '../Components/add-vendor/add-vendor.component';
-import { PersonalDetailsComponent } from '../Components/personal-details/personal-details.component';
 import { StaffDetailsComponent } from '../Components/staff-details/staff-details.component';
 import { TechnicalDetailsComponent } from '../Components/technical-details/technical-details.component';
 import { PageNotFoundComponent } from '../Components/page-not-found/page-not-found.component';
@@ -12,26 +11,13 @@ import { DocumentComponent } from '../Components/document/document.component';
 import { BankDetailsComponent } from '../Components/bank-details/bank-details.component';
 import { LoginComponent } from '../Components/login/login.component';
 import { WelcomeComponent } from '../Components/welcome/welcome.component';
+import { PersonalDetailsComponent } from '../Components/personal-details/personal-details.component';
 
 const routes: Routes = [
-
-  {
-    path: 'vendor',
-    component: VendorListComponent
-  },
-  {
-    path: 'vendor/add',
-    component: AddVendorComponent,
-    children: [
-      { path: 'personal', component: PersonalDetailsComponent },
-      { path: 'staff', component: StaffDetailsComponent },
-      { path: 'technical', component: TechnicalDetailsComponent },
-      { path: 'production', component: ProductionDetailsComponent },
-      { path: 'business', component: BusinessDetailsComponent },
-      { path: 'document', component: DocumentComponent },
-      { path: 'bank', component: BankDetailsComponent }
-    ]
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'vendor', component: VendorListComponent },
   {
     path: 'vendor/:code',
     component: AddVendorComponent,
@@ -47,18 +33,26 @@ const routes: Routes = [
     ]
   },
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
+    path: '**', component: PageNotFoundComponent
   }
+
+  // {
+  //   path: 'login',
+  //   component: LoginComponent
+  // },
+  // {
+  //   path: 'welcome',
+  //   component: WelcomeComponent
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class LoginModule { }
