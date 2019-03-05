00f66a32d780232b2e350bb9046651b3a397ce65
Shubhi-J <45873056+Shubhi-J@users.noreply.github.com>
2019-03-05 18:54:39 +0530

    staff Detail model added

---
+++ src/app/Models/staff-details.ts
@@ -0,0 +1,13 @@
+export class StaffDetails {
+    RowNumber: number;
+    Designation: number;
+    ContactName: string;
+    DepartmentList: string;
+    DeptList: string;
+    ConfigIdsList: string;
+    StaffIdsList: string;
+    ContactEmail: string;
+    ContactPhone: string;
+    Remarks: string;
+    Priority: number;
+}
