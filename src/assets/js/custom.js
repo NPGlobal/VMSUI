f27053ffc99ae65c6266753a302e3292edcc68ed
anuragparashar1 <45871807+anuragparashar1@users.noreply.github.com>
2019-07-22 10:15:30 +0530

    changes in responsive design

---
+++ src/assets/js/custom.js
@@ -0,0 +1,45 @@
+/** 
+  8. HOVER DROPDOWN MENU
+  9. SCROLL TOP BUTTON
+  10. BUTTON SMOOTH SCROLL ( VIEW RESERVATION FORM ) 
+**/
+
+jQuery(function($){
+	
+  /* ----------------------------------------------------------- */
+  /*  8. HOVER DROPDOWN MENU
+  /* ----------------------------------------------------------- */ 
+  
+  // for hover dropdown menu
+    jQuery('ul.nav li.dropdown').hover(function() {
+      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+
+    
+  /* ----------------------------------------------------------- */
+  /*  9. SCROLL TOP BUTTON
+  /* ----------------------------------------------------------- */
+
+  //Check to see if the window is top if not then display button
+
+    jQuery(window).scroll(function(){
+      if (jQuery(this).scrollTop() > 300) {
+        jQuery('.scrollToTop').fadeIn();
+      } else {
+        jQuery('.scrollToTop').fadeOut();
+      }
+    });
+     
+    //Click event to scroll to top
+
+    jQuery('.scrollToTop').click(function(){
+      jQuery('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+  
+  
+  
+});
+
